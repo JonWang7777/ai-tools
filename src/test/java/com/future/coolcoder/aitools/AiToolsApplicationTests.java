9e6d466e97ba12c9def4f67d2b236b9989fd0d42
Jon <jonwang7777@gmail.com>
2023-08-26 10:19:47 +0800

    init project

---
+++ src/test/java/com/future/coolcoder/aitools/AiToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.future.coolcoder.aitools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
